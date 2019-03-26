b74d9860e30338662a2aa082c57977c2530c84e7
joe-michel <joe.michel.f@gmail.com>
2019-03-26 09:55:49 +0100

    Updates

---
+++ precache-manifest.102597f074f91475151b3114cbac1ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d242375651bc27cc5a1",
+    "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
+  },
+  {
+    "revision": "4eb06adb85c504888a40",
+    "url": "/react-character-manager/static/js/main.8d4c39dc.chunk.js"
+  },
+  {
+    "revision": "589400bcba7c3981df99",
+    "url": "/react-character-manager/static/js/2.fb961e1c.chunk.js"
+  },
+  {
+    "revision": "4eb06adb85c504888a40",
+    "url": "/react-character-manager/static/css/main.3c317f53.chunk.css"
+  },
+  {
+    "revision": "589400bcba7c3981df99",
+    "url": "/react-character-manager/static/css/2.bccd7db9.chunk.css"
+  },
+  {
+    "revision": "c9292f9f6b9c60c9365c299ff923cac0",
+    "url": "/react-character-manager/index.html"
+  }
+];
