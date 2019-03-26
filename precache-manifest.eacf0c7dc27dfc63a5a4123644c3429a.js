e00a16f5f609e42e412fe3dd09a7be8eb4fff828
joe-michel <joe.michel.f@gmail.com>
2019-03-26 14:07:17 +0100

    Updates

--- precache-manifest.eacf0c7dc27dfc63a5a4123644c3429a.js
+++ precache-manifest.eacf0c7dc27dfc63a5a4123644c3429a.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/react-character-manager/static/js/runtime~main.0c53a67b.js"
   },
   {
-    "revision": "06f2676bea9ee6efad1a",
-    "url": "/react-character-manager/static/js/main.391cf609.chunk.js"
+    "revision": "1999fb0b7d2f4d407406",
+    "url": "/react-character-manager/static/js/main.6c3611b9.chunk.js"
   },
   {
-    "revision": "2619a163fb10f8805ac8",
-    "url": "/react-character-manager/static/js/2.493899ae.chunk.js"
+    "revision": "bfdd45e145c2158815a3",
+    "url": "/react-character-manager/static/js/2.45ea12ba.chunk.js"
   },
   {
-    "revision": "06f2676bea9ee6efad1a",
+    "revision": "1999fb0b7d2f4d407406",
     "url": "/react-character-manager/static/css/main.3c317f53.chunk.css"
   },
   {
-    "revision": "2619a163fb10f8805ac8",
+    "revision": "bfdd45e145c2158815a3",
     "url": "/react-character-manager/static/css/2.bccd7db9.chunk.css"
   },
   {
-    "revision": "c3fdd26f0ea6d4a8e22156904b33f48c",
+    "revision": "398c2b879647e82f6560215da7166397",
     "url": "/react-character-manager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-character-manager/precache-manifest.717bb6d0ad12168f3b80020369243352.js"
+  "/react-character-manager/precache-manifest.eacf0c7dc27dfc63a5a4123644c3429a.js"
 );
 
 workbox.clientsClaim();
